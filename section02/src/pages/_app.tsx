0dc1b523c9a77cb7d0ddbfb8abb597b060da319d
Yean <kjan0929@naver.com>
2024-10-13 17:09:16 +0900

    style: 글로벌 레이아웃 설정

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,11 +1,11 @@
+import GlobalLayout from "@/components/global-layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <header>글로벌 헤더</header>
+    <GlobalLayout>
       <Component {...pageProps} />
-    </>
+    </GlobalLayout>
   );
 }
