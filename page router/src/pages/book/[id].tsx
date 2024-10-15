fe24ab11bf4e002b36d7a5ed5f1c32ff4549a01f
Yean <kjan0929@naver.com>
2024-10-15 23:29:41 +0900

    feat: SEO 설정

--- page router/src/pages/book/[id].tsx
+++ page router/src/pages/book/[id].tsx
@@ -2,6 +2,7 @@ import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import style from "./[id].module.css";
 import fetchOneBook from "@/lib/fetch-one-book";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 export const getStaticPaths = () => {
   return {
@@ -13,7 +14,7 @@ export const getStaticPaths = () => {
     fallback: true,
     // false: 404 not found
     // blocking : SSR 방식
-    // true: Props 데이터가 없는 fall back 상태의 페이지 반환 + SSR로 데이터 후속으로 넣어주기
+    // true: Props 데이터가 없는 fall back 상태의 페이지 우선 반환 + SSR로 데이터 후속으로 넣어주기
   };
 };
 
@@ -38,27 +39,50 @@ export default function Page({
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   const router = useRouter();
 
-  if (router.isFallback) return "로딩 중입니다";
+  if (router.isFallback) {
+    return (
+      <>
+        <Head>
+          <title>한입북스</title>
+          <meta property="og:image" content="/thumbnail.png" />
+          <meta property="og:title" content="한입북스" />
+          <meta
+            property="og:description"
+            content="한입북스에 등록된 도서들을 만나보세요"
+          />
+        </Head>
+        <div>로딩 중입니다</div>
+      </>
+    );
+  }
   if (!book) return "문제가 발생했습니다 다시 시도하세요";
 
   const { id, title, subTitle, description, author, publisher, coverImgUrl } =
     book;
 
   return (
-    <div className={style.container}>
-      <div
-        className={style.cover_img_container}
-        style={{ backgroundImage: `url('${coverImgUrl}')` }}
-      >
-        <img src={coverImgUrl} alt="cover-image" />
-      </div>
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta property="og:image" content={coverImgUrl} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+      </Head>
+      <div className={style.container}>
+        <div
+          className={style.cover_img_container}
+          style={{ backgroundImage: `url('${coverImgUrl}')` }}
+        >
+          <img src={coverImgUrl} alt="cover-image" />
+        </div>
 
-      <div className={style.title}>{title}</div>
-      <div className={style.subTitle}>{subTitle}</div>
-      <div className={style.author}>
-        {author} | {publisher}
+        <div className={style.title}>{title}</div>
+        <div className={style.subTitle}>{subTitle}</div>
+        <div className={style.author}>
+          {author} | {publisher}
+        </div>
+        <div className={style.description}>{description}</div>
       </div>
-      <div className={style.description}>{description}</div>
-    </div>
+    </>
   );
 }
--- page router/src/pages/search/index.tsx
+++ page router/src/pages/search/index.tsx
@@ -1,26 +1,10 @@
 import SearchableLayout from "@/components/searchable-layout";
 import { ReactNode, useEffect, useState } from "react";
 import BookItem from "@/components/book-item";
-import {
-  GetServerSidePropsContext,
-  GetStaticPropsContext,
-  InferGetServerSidePropsType,
-} from "next";
 import fetchBooks from "@/lib/fetch-books";
 import { useRouter } from "next/router";
 import { BookData } from "@/types";
-
-// export const getStaticProps = async (context: GetStaticPropsContext) => {
-//   // context에는 현재 브라우저로부터 받은 요청에 대한 모든 정보가 담겨있음
-//   const q = context.query.q;
-//   const books = await fetchBooks(q as string);
-
-//   return {
-//     props: {
-//       books,
-//     },
-//   };
-// };
+import Head from "next/head";
 
 export default function SearchPage() {
   const [books, setBooks] = useState<BookData[]>([]);
@@ -40,11 +24,22 @@ export default function SearchPage() {
   }, [q]);
 
   return (
-    <div>
-      {books.map((book) => (
-        <BookItem key={book.id} {...book} />
-      ))}
-    </div>
+    <>
+      <Head>
+        <title>한입북스 - 검색결과</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="한입북스 - 검색결과" />
+        <meta
+          property="og:description"
+          content="한입북스에 등록된 도서들을 만나보세요"
+        />
+      </Head>
+      <div>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </div>
+    </>
   );
 }
 
