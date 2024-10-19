34ed483ca4ba76b8b02d450b3070f58d45e21e4e
Yean <kjan0929@naver.com>
2024-10-19 17:57:06 +0900

    chore: legacy 디렉토리 삭제

--- app-router/src/app/layout.tsx
+++ app-router/src/app/layout.tsx
@@ -1,7 +1,26 @@
 import "./globals.css";
 import Link from "next/link";
 import style from "./layout.module.css";
+import { BookData } from "@/types";
 
+async function Footer() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`
+  );
+  if (!response.ok) {
+    return <footer>제작 @vlmbuyd</footer>;
+  }
+
+  const books: BookData[] = await response.json();
+  const bookCount = books.length;
+
+  return (
+    <footer>
+      <div>제작 @vlmbuyd</div>
+      <div>{bookCount}개의 도서가 등록되어 있습니다</div>
+    </footer>
+  );
+}
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -15,7 +34,7 @@ export default function RootLayout({
             <Link href={"/"}>📚 ONEBITE BOOKS</Link>
           </header>
           <main>{children}</main>
-          <footer>제작 @winterlood</footer>
+          <Footer />
         </div>
       </body>
     </html>
