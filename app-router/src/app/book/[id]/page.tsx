5e399dff58b07d1601e44eaecf072e4148ae10f0
Yean <kjan0929@naver.com>
2024-11-06 23:48:54 +0900

    feat: 리뷰 조회 기능 구현

--- app-router/src/app/book/[id]/page.tsx
+++ app-router/src/app/book/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { notFound } from "next/navigation";
 import style from "./page.module.css";
-import { createReviewAction } from "@/actions/create-review.action";
+import { ReviewData } from "@/types";
+import ReviewItem from "@/components/review-item";
+import ReviewEditor from "@/components/review-editor";
 
 // generateStaticParams에서 설정한 url 파라미터 외에는 404 페이지로 리다이렉트 시키고 싶다면..
 // export const dynamicParams = false;
@@ -45,15 +47,22 @@ async function BookDetail({ bookId }: { bookId: string }) {
   );
 }
 
-function ReviewEditor({ bookId }: { bookId: string }) {
+async function ReviewList({ bookId }: { bookId: string }) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review/book/${bookId}`
+  );
+
+  if (!response.ok) {
+    throw new Error(`Reveiw fetch failed: ${response.statusText}`);
+  }
+
+  const reviews: ReviewData[] = await response.json();
+
   return (
     <section>
-      <form action={createReviewAction}>
-        <input name="bookId" value={bookId} hidden readOnly />
-        <input required name="content" placeholder="리뷰 내용" />
-        <input required name="author" placeholder="작성자" />
-        <button type="submit">작성하기</button>
-      </form>
+      {reviews.map((review) => (
+        <ReviewItem key={`review-item-${review.id}`} {...review} />
+      ))}
     </section>
   );
 }
@@ -70,6 +79,7 @@ export default async function Page({ params }: BookPageProps) {
     <div className={style.container}>
       <BookDetail bookId={resolvedParams.id} />
       <ReviewEditor bookId={resolvedParams.id} />
+      <ReviewList bookId={resolvedParams.id} />
     </div>
   );
 }
