8bb365f41c86b869bf696adee4637c75e7bbc0eb
Yean <kjan0929@naver.com>
2024-11-09 22:54:01 +0900

    feat: revalidatePath 활용한 리뷰 실시간 반영

--- app-router/src/actions/create-review.action.ts
+++ app-router/src/actions/create-review.action.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+
 export async function createReviewAction(formData: FormData) {
   const bookId = formData.get("bookId")?.toString();
   const content = formData.get("content")?.toString();
@@ -16,6 +18,10 @@ export async function createReviewAction(formData: FormData) {
       }
     );
     console.log(response.status);
+
+    // 경로에 해당하는 페이지(내부에 있는 요소 모두) 재생성(재검증) 요청 -> 리뷰 데이터 실시간 반영
+    // 서버 측에서만 요청 가능 -> 서버 액션, 서버 컴포넌트에서만 요청 가능
+    revalidatePath(`/book/${bookId}`);
   } catch (error) {
     console.error(error);
     return;
