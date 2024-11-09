fde714e21dee307758bd95bfd902a7b30930b64d
Yean <kjan0929@naver.com>
2024-11-09 23:52:11 +0900

    feat: useActionState를 활용한 서버 액션 상태 관리

--- app-router/src/actions/create-review.action.ts
+++ app-router/src/actions/create-review.action.ts
@@ -1,13 +1,18 @@
 "use server";
 
-import { revalidatePath, revalidateTag } from "next/cache";
+import { revalidateTag } from "next/cache";
 
-export async function createReviewAction(formData: FormData) {
+export async function createReviewAction(_: any, formData: FormData) {
   const bookId = formData.get("bookId")?.toString();
   const content = formData.get("content")?.toString();
   const author = formData.get("author")?.toString();
 
-  if (!bookId || !content || !author) return;
+  if (!bookId || !content || !author) {
+    return {
+      status: false,
+      error: "리뷰 내용과 작성자를 입력해주세요",
+    };
+  }
 
   try {
     const response = await fetch(
@@ -17,11 +22,18 @@ export async function createReviewAction(formData: FormData) {
         body: JSON.stringify({ bookId, content, author }),
       }
     );
-    console.log(response.status);
-
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
     revalidateTag(`review-${bookId}`);
+    return {
+      status: true,
+      error: "",
+    };
   } catch (error) {
-    console.error(error);
-    return;
+    return {
+      status: false,
+      error: `리뷰 저장에 실패했습니다 : ${error}`,
+    };
   }
 }
