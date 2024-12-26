6c6d34b046be5cbd428ba41e9db6a6d1ee9059b6
Yean <kjan0929@naver.com>
2024-12-26 22:51:59 +0900

    setting: parallel route 설정

--- X/src/app/(beforeLogin)/layout.tsx
+++ X/src/app/(beforeLogin)/layout.tsx
@@ -1,13 +1,16 @@
 import {ReactNode} from "react";
 
 type Props = { children: ReactNode, modal: ReactNode };
-export default function Layout({ children }: Props) {
+
+export default function Layout({ children, modal }: Props) {
   return (
     <div>
+      비포 로그인 레이아웃
       {children}
+      {modal}
     </div>
   )
 }
 
-// 주소가 localhost:3001일 때는 children->page.tsx, modal->@modal/default.tsx
-// 주소가 localhost:3001/i/flow/login 때는 chldren->i/flow/login/page.tsx, modal->@modal/i/flow/login/page.tsx
+// 주소가 localhost:3000일 때는 children->page.tsx, modal->@modal/default.tsx
+// 주소가 localhost:3000/i/flow/login 때는 chldren->i/flow/login/page.tsx, modal->@modal/i/flow/login/page.tsx
